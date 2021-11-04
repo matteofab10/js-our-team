241a9fb534945bfae45f4aa6eef79cfc19768978
matteofab10 <matteofabio10@gmail.com>
2021-11-04 15:05:59 +0100

    commit creazione progetto

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.team-container');
+
+const profili = [
+{
+  name : 'Angela Caroll',
+  job : 'Chief Editor',
+  img : ''
+},
+{
+  name : 'Walter Gordon',
+  job : 'Office Manager',
+  img : ''
+},
+{
+  name : 'Angela Lopez',
+  job : 'Social Media Manager',
+  img : ''
+},
+{
+  name : 'Scott Estrada',
+  job : 'Developer',
+  img : ''
+},
+{
+  name : 'Barbara Ramos',
+  job : 'Graphic Designer',
+  img : ''
+}]
+
+console.log(profili);
